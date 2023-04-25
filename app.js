ae372889c8948f498340692f3daef92f65cc7559
azharrahad31 <azherudinrahad@gmail.com>
2023-04-26 00:01:48 +0600

    random code generate

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-hello
+function createRandomNumber(){
+    const randomNumber=Math.round(Math.random()*10000);
+    const randomString= randomNumber + "";
+    if(randomString.length === 4){
+        return randomString;   
+    }
+    else{
+       return createRandomNumber()
+    }
+}
+
+ document.querySelector('#generatePin-btn').addEventListener('click',function(){
+    const pinNumber=createRandomNumber();
+    const pinInputField=document.querySelector('#display-pin');
+    pinInputField.value=pinNumber;
+
+
+ });
+
+
+ ///event delegation in javascript
+
+ document.querySelector('#calculator').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    const numberInputField=document.querySelector('#input-number')
+    numberInputField.value=number;
+})
