a4585dda57b2d8155be1f5c18cb562b85633d4bf
azharrahad31 <azherudinrahad@gmail.com>
2023-04-26 09:49:20 +0600

    if pin match show a success full message

--- app.js
+++ app.js
@@ -13,15 +13,52 @@ function createRandomNumber(){
     const pinNumber=createRandomNumber();
     const pinInputField=document.querySelector('#display-pin');
     pinInputField.value=pinNumber;
-
-
  });
-
-
  ///event delegation in javascript
-
  document.querySelector('#calculator').addEventListener('click',function(event){
     const number = event.target.innerText;
-    const numberInputField=document.querySelector('#input-number')
-    numberInputField.value=number;
+    const numberInputField=document.querySelector('#input-number'); 
+    if(isNaN(number)){
+    if(number === 'C'){
+        numberInputField.value = " ";
+    }
+    else if (number === '<'){
+      const inputFieldNumber= numberInputField.value;
+      const stringArray=inputFieldNumber.split('');
+      stringArray.pop();
+      const makeString=stringArray.join('');
+      numberInputField.value=makeString;
+      console.log(stringArray);
+    }
+ }
+   else{
+    numberInputField.value += number;
+   }
+
+})
+
+function pinMechlin (){
+    const pinInputField=document.querySelector('#display-pin');
+    const pinValue=pinInputField.value;
+    const numberInputField=document.querySelector('#input-number');
+    const numberValue=numberInputField.value;
+    const pinSuccess=document.querySelector('#pin-success');
+    const pinfille=document.querySelector('#pin-failure');
+    
+    if(pinValue === numberValue){
+        pinSuccess.style.display='block';
+        pinfille.style.display='none';
+
+    }
+    else{
+       pinfille.style.display='block';
+       pinSuccess.style.display='none';
+
+    }
+}
+
+
+document.querySelector('#veify-pin').addEventListener('click',function(){
+    pinMechlin ()
+
 })
